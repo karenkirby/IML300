7fd650584370864e3e4728cf0c9f8298adf1be9e
karenswan <karenwang97@outlook.com>
2020-10-15 16:00:19 -0700

    final

--- Project-2/p5js-Bath Bomb Effects-ChromeExtension/sketch.js
+++ Project-2/p5js-Bath Bomb Effects-ChromeExtension/sketch.js
@@ -1,5 +1,6 @@
-
 //inspiration: perlin noise by yasai: https://www.openprocessing.org/sketch/494102
+
+
 
 var particles_a = [];
 var particles_b = [];
@@ -9,30 +10,64 @@ var particles_e = [];
 var particles_f = [];
 var particles_g = [];
 
-var nums =300;
-var noiseScale = 270;
+var nums =400;
+var noiseScale = 300;
 
 function setup(){
+
 	createCanvas(windowWidth, windowHeight);
+ 
 	background(219, 236, 245);
+
     for(var i = 0; i < nums; i++){
-		particles_a[i] = new Particle(random(0, width/5),random(0,height/5));
-		particles_b[i] = new Particle(random(0, width/5),random(0,height/5));
-		particles_c[i] = new Particle(random(0, width/5),random(0,height/5));
-      particles_d[i] = new Particle(random(0, width/5),random(0,height/5));
-      particles_e[i] = new Particle(random(0, width/5),random(0,height/5));
-      particles_f[i] = new Particle(random(0, width/5),random(0,height/5));
-      particles_g[i] = new Particle(random(0, width/5),random(0,height/5));
+		particles_a[i] = new Particle(random(0,width/2),random(0, height/2));
+		particles_b[i] = new Particle(random(width/2, width),random(height/2,height));
+		particles_c[i] = new Particle(random(0,width/2,),random(height/2,height));
+      particles_d[i] = new Particle(random(0, width),random(0, height));
+      particles_e[i] = new Particle(random(width/2,width),random(0,height/2));
+      particles_f[i] = new Particle(random(0, width),random(0, height));
+      particles_g[i] = new Particle(random(2*width/7, 4*width/7),random(2*height/7, 4.5*height/7));
       
 	}
 
 }
 
+
 function draw(){
 
+ strokeWeight(8);
+    fill(255);
+    textSize(40);
+    textStyle(NORMAL);
+  text("繋げる", 180,windowHeight/2-25);
+  text("连接", 100,windowHeight/2);
+  text("Connect", 180,windowHeight/2+30);
+  text("Collegare", 220,windowHeight/2-90);
+  text("conectar", 320,windowHeight/2+50);
+  
+  
+  
+  textSize(25);
+  text("menghubungkan", 200,windowHeight/2);
+  text("relier", 240,windowHeight/2-60);
+  text("मिलना", 310,windowHeight/2-30);
+  text("подключиться", 120,windowHeight/2+50);
+  text("verbinden", 170,windowHeight/2+80);
 
+  
+
+  
+  
+  
+  
+    textSize(25);
+    textStyle(NORMAL);
+    strokeWeight(5);
+  
+  
 	noStroke();
 	smooth();
+  
   
   
   for(var i = 0; i < nums; i++){
@@ -68,7 +103,7 @@ function draw(){
 	}  
   
     if (mouseIsPressed === true) {
-    background(13, 21, 115)
+    background(0,0,0)
   for(var i = 0; i < nums; i++){
 		var radius = map(i,0,nums,1,2);
 		var alpha = map(i,0,nums,0,250);
@@ -82,7 +117,29 @@ function draw(){
 		particles_f[i].move();
 		particles_f[i].display(radius);
 		particles_f[i].checkEdge();
+    
+     
+    fill(180, 0, 0, 51);
+    textSize(40);
+    textStyle(NORMAL);
+  text("繋げる", 180,windowHeight/2-25);
+  text("连接", 100,windowHeight/2);
+  text("Connect", 180,windowHeight/2+30);
+  text("Collegare", 220,windowHeight/2-90);
+  text("conectar", 320,windowHeight/2+50);
+  
+  
+  
+  textSize(25);
+  text("menghubungkan", 200,windowHeight/2);
+  text("relier", 240,windowHeight/2-60);
+  text("मिलना", 310,windowHeight/2-30);
+  text("подключиться", 120,windowHeight/2+50);
+  text("verbinden", 170,windowHeight/2+80);
+    
     }
+      
+
     }
   
   // for(var x = 0; x < nums/3; x++){
@@ -124,6 +181,5 @@ function Particle(x, y){
 	this.display = function(r){
 		ellipse(this.pos.x, this.pos.y, r, r);
 	}
-  
-
 }
+
